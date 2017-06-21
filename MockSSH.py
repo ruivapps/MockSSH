41bc26ae4033daef25b0ae9e62c03e8039b403d3
Rui Li <rui.li@logmein.com>
2017-06-21 15:56:33 -0400

    fix exec_command

--- MockSSH.py
+++ MockSSH.py
@@ -118,10 +118,7 @@ class ArgumentValidatingCommand(SSHCommand):
         self.name = name
         self.success_callbacks = success_callbacks
         self.failure_callbacks = failure_callbacks
-        if isinstance(args[0], list):
-            self.required_arguments = [[name] + list(x) for x in args]
-        else:
-            self.required_arguments = [name] + list(args)
+        self.required_arguments = [name] + list(args)
         self.protocol = None  # set in __call__
 
     def __call__(self, protocol, *args):
@@ -129,7 +126,7 @@ class ArgumentValidatingCommand(SSHCommand):
         return self
 
     def start(self):
-        if not list(self.args) == self.required_arguments:
+        if not tuple(self.args) == tuple(self.required_arguments):
             [func(self) for func in self.failure_callbacks]
         else:
             [func(self) for func in self.success_callbacks]
@@ -320,27 +317,7 @@ class SSHAvatar(avatar.ConchUser):
         return None
 
     def execCommand(self, protocol, cmd):
-        if cmd:
-            self.client = TransportWrapper(protocol)
-
-            cmd_and_args = cmd.split()
-            cmd, args = cmd_and_args[0], cmd_and_args[1:]
-            func = self.get_exec_func(cmd)
-
-            if func:
-                try:
-                    func(*args)
-                except Exception as e:
-                    self.client.write("Error: {0}".format(e))
-            else:
-                self.client.write("No such command.")
-
-            self.client.loseConnection()
-            protocol.session.conn.transport.expectedLoseConnection = 1
-
-        #raise NotImplemented
-    def get_exec_func(self, cmd):
-        return getattr(self, 'exec_' + cmd, None)
+        raise NotImplemented
 
     def closed(self):
         pass
@@ -534,29 +511,6 @@ def startThreadedServer(commands,
 def stopThreadedServer():
     reactor.callFromThread(reactor.stop)
 
-class TransportWrapper(object):
-
-    def __init__(self, p):
-        self.protocol = p
-        p.makeConnection(self)
-        self.closed = False
-
-    def write(self, data):
-        self.protocol.outReceived(data)
-        self.protocol.outReceived('\r\n')
-
-        # Mimic 'exit' for the shell test
-        if '\x00' in data:
-            self.loseConnection()
-
-    def loseConnection(self):
-        if self.closed:
-            return
-
-        self.closed = True
-        self.protocol.inConnectionLost()
-        self.protocol.outConnectionLost()
-        self.protocol.errConnectionLost()
 
 if __name__ == "__main__":
     users = {'root': 'x'}
